5ba29b2a5ef5b73088c7c6b6c609e90ffa453842
guerfala <mohamedtaher.guerfala@esprit.tn>
2024-10-24 14:28:05 +0100

    First view

--- app/src/main/java/com/example/jardinenfantmobile/MainActivity.java
+++ app/src/main/java/com/example/jardinenfantmobile/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.jardinenfantmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //Titre
+        getSupportActionBar().setTitle("Jardin Enfant");
+
+        //Login Activity
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Register Activity
+        Button buttonRegister = findViewById(R.id.button_register);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
