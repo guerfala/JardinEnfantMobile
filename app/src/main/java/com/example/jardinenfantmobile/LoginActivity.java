5abc7dc054d9674aaa6172173a04fa9017bd5fd4
guerfala <mohamedtaher.guerfala@esprit.tn>
2024-11-01 13:03:37 +0100

    Mail verification for login

--- app/src/main/java/com/example/jardinenfantmobile/LoginActivity.java
+++ app/src/main/java/com/example/jardinenfantmobile/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.jardinenfantmobile;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -26,6 +29,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -103,7 +107,20 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    Toast.makeText(LoginActivity.this, "You are logged in now", Toast.LENGTH_SHORT).show();
+
+                    //get instance of the current user
+                    FirebaseUser firebaseUser = authProfile.getCurrentUser();
+
+                    //check if email is verified
+                    if (firebaseUser.isEmailVerified()){
+                        Toast.makeText(LoginActivity.this, "You are logged in now", Toast.LENGTH_SHORT).show();
+
+                    }else {
+                        firebaseUser.sendEmailVerification();
+                        authProfile.signOut();
+                        showAlertDialog();
+                    }
+
                 } else {
                     try {
                         throw task.getException();
@@ -122,5 +139,27 @@ public class LoginActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.GONE);
             }
         });
+    }
+
+    private void showAlertDialog() {
+        //setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        builder.setTitle("Email is not verified");
+        builder.setMessage("Please verify your email now. You can not login without email verification.");
+
+        builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        //create the alertDialog
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
     }
 }
