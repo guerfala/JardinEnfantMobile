45da5bcf8739fd0043430eb09a764f4464ce5fdf
Med Rami Brick <brickrami1@gmail.com>
2024-11-10 11:41:04 +0100

    prepare for user merge

--- app/src/main/java/com/example/jardinenfantmobile/UserProfileActivity.java
+++ app/src/main/java/com/example/jardinenfantmobile/UserProfileActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -22,7 +21,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.example.jardinenfantmobile.Student.CreateStudentActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -39,18 +37,11 @@ public class UserProfileActivity extends AppCompatActivity {
     private ImageView imageView;
     private FirebaseAuth authProfile;
     private SwipeRefreshLayout swipeContainer;
-    private Button buttonAddStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        buttonAddStudent = findViewById(R.id.buttonAddStudent);
-
-        buttonAddStudent.setOnClickListener(v -> {
-            Intent intent = new Intent(UserProfileActivity.this, CreateStudentActivity.class);
-            startActivity(intent);
-        });
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
